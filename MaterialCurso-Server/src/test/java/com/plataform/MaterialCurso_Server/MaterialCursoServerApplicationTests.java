a111b7a88ee2a8e718199836b974dfb30cbeb4b1
Melo-05 <tu-correo@ejemplo.com>
2026-05-19 21:17:57 -0500

    MaterialCurso-Server

---
+++ MaterialCurso-Server/src/test/java/com/plataform/MaterialCurso_Server/MaterialCursoServerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.plataform.MaterialCurso_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaterialCursoServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
